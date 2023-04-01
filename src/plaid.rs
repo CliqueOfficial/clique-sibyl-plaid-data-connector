cdca08f8ec03f020844f0ded39f6cef7ee56e694
DCMMC <xwt97294597@gmail.com>
2023-04-01 09:30:49 +0000

    add simple_tls_client_no_cert_check

--- src/plaid.rs
+++ src/plaid.rs
@@ -9,7 +9,7 @@ use std::panic;
 use std::time::*;
 // use std::untrusted::time::SystemTimeEx;
 use sibyl_base_data_connector::utils::{parse_result_chunked, parse_result, tls_post};
-use sibyl_base_data_connector::utils::simple_tls_client;
+use sibyl_base_data_connector::utils::{simple_tls_client, simple_tls_client_no_cert_check};
 
 // Plaid API
 
@@ -178,7 +178,7 @@ impl DataConnector for PlaidConnector {
                                     query_param["rsaPubkey"].as_str().unwrap_or(""),
                                     SIGN_CLAIM_SGX_HOST
                                 );
-                                let zk_range_proof = simple_tls_client(SIGN_CLAIM_SGX_HOST, &req, 12341).unwrap_or(json!({"result": {}}));
+                                let zk_range_proof = simple_tls_client_no_cert_check(SIGN_CLAIM_SGX_HOST, &req, 12341).unwrap_or(json!({"result": {}}));
                                 let zk = &zk_range_proof["result"];
                                 return json!({
                                     "result": in_range,

6808971231cc8c872dc12f75e16b291cac3f6029
will-beta <i@will-beta.com>
2023-10-27 11:09:49 +0800

    fix private constant variables access issue

--- connectors/SibylPlaidDataConnector/src/env.rs
+++ connectors/SibylPlaidDataConnector/src/env.rs
@@ -1,9 +1,9 @@
-const SIGN_CLAIM_SGX_HOST: &'static str = "clique-sign-claim";
+pub const SIGN_CLAIM_SGX_HOST: &'static str = "clique-sign-claim";
 
 // Plaid API
-const BALANCE_SUFFIX: &'static str = "/accounts/balance/get";
-const LINK_TOKEN_SUFFIX: &'static str = "/link/token/create";
-const EXCHANGE_ACCESS_TOKEN_SUFFIX: &'static str = "/item/public_token/exchange";
-const SANDBOX_PUBLIC_TOKEN_SUFFIX: &'static str = "/sandbox/public_token/create";
-const SANDBOX_EXCHANGE_ACCESS_TOKEN_SUFFIX: &'static str = "/item/public_token/exchange";
-const SANDBOX_PLAID_HOST: &'static str = "sandbox.plaid.com";
+pub const BALANCE_SUFFIX: &'static str = "/accounts/balance/get";
+pub const LINK_TOKEN_SUFFIX: &'static str = "/link/token/create";
+pub const EXCHANGE_ACCESS_TOKEN_SUFFIX: &'static str = "/item/public_token/exchange";
+pub const SANDBOX_PUBLIC_TOKEN_SUFFIX: &'static str = "/sandbox/public_token/create";
+pub const SANDBOX_EXCHANGE_ACCESS_TOKEN_SUFFIX: &'static str = "/item/public_token/exchange";
+pub const SANDBOX_PLAID_HOST: &'static str = "sandbox.plaid.com";
